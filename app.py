e02c9aa7711911c39031458461a0de2fb4df69a8
noobpalabr <muhammad.naufal.abrari@gmail.com>
2024-07-09 14:16:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -33,4 +34,5 @@ def cluster():
     return render_template('index.html', result=u_pred)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
